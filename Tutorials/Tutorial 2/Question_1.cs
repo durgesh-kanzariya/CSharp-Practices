46612b657087c0b79ebf0eade604d3217a337c03
Durgesh Kanzariya <durgesh.j.kanzariya@gmail.com>
2025-08-21 20:50:55 +0530

    Tutorial 2 first 3 questions completed

---
+++ Tutorials/Tutorial 2/Question_1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23SOEIT11008_DURGESH_KANZARIYA.Tutorials.Tutorial_2
+{
+    internal class Question_1
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                /* local variable definition */
+                int a = 10;
+                /* check the boolean condition using if statement */
+                if (a < 20)
+                {
+                    /* if condition is true then print the following */
+                    Console.WriteLine("a is less than 20");
+                }
+                Console.WriteLine("value of a is : {0}", a);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Tutorials/Tutorial 2/Question_3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23SOEIT11008_DURGESH_KANZARIYA.Tutorials.Tutorial_2
+{
+    internal class Question_3
+    {
+        static void Main(string[] args)
+        {
+            String firstName = "John";
+            String lastName = "Doe";
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+            Console.WriteLine("Please enter a new first name:");
+            firstName = Console.ReadLine();
+            Console.WriteLine("New name: " + firstName + " " + lastName);
+            Console.ReadLine();
+        }
+    }
+}
